fdf63d67564f26c93ee7b72ac784f74cb530063c
Mathew Woodyard <mat@matwoodyard.com>
2020-08-09 00:05:36 -0500

    Properly support running the role without creating a profile and a .aws/ directory. Add support for uninstalling the AWS CLI tool.

---
+++ molecule/uninstall/tests/test_uninstall.py
@@ -0,0 +1,10 @@
+import os
+import testinfra.utils.ansible_runner
+import pytest
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_aws_cli_should_not_be_installed(host):
+    assert host.run('aws --v').rc != 0
